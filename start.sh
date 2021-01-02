4cf06b2c8e05ffeb6ce4bd5ab4d3f4d59dd30681
clovernet <760450699@qq.com>
2021-01-02 16:43:32 +0000

    提交

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /usr/share/nginx/html/ &
 #获取qqcookie
 curl -A 'Mozilla/5.0 (Windows NT 6.1)' https://api.qq.jsososo.com/user/cookie | jq '.data.userCookie' >> apiQ/data/cookie.json &  
 #替换cookie中的qq号
-sed -i 's/'1234567'/'$(cat data/cookie.json | jq .uin)'/g' apiQ/bin/config.js &
+sed -i 's/'1234567'/'$(cat apiQ/data/cookie.json | jq .uin)'/g' apiQ/bin/config.js &
 cd /usr/share/nginx/html/apiQ && nohup npm start & 
 cd /usr/share/nginx/html/api && nohup node app.js & 
 cd /usr/share/nginx/html/apiMix && nohup npm start & 
