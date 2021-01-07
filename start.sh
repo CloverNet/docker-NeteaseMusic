ed21737b6e7de521aef1c5f745e20cd2885799ca
jyx <760450699@qq.com>
2021-01-07 21:52:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /usr/share/nginx/html/ &
 #获取qqcookie
+rm apiQ/data/cookie.json & 
 curl -A 'Mozilla/5.0 (Windows NT 6.1)' https://api.qq.jsososo.com/user/cookie | jq '.data.userCookie' >> apiQ/data/cookie.json &  
 #替换cookie中的qq号
 sed -i 's/'1234567'/'$(cat apiQ/data/cookie.json | jq .uin)'/g' apiQ/bin/config.js &
